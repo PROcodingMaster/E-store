ffa4e525ef9efaf0a3ffd0399e271ffc97bf9090
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-30 23:43:03 +0530

    Header created

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!--------------------------------- NAVBAR Start --------------------------------->
+<link rel="stylesheet" href="css/stylesheet.css">
+<nav class="navbar navbar-default navbar-fixed-top navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">E-Store</a>
+        <div class="collapse navbar-collapse ml-auto" id="navbarNavAltMarkup">
+            <div class="navbar-nav ">
+                <a class="nav-item nav-link active" href="signup.php"><span class="glyphicon glyphicon-user"></span>Sign Up </a>
+                <a class="nav-item nav-link" href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a>
+                <a class="nav-item nav-link" href="about.php"><span class="glyphicon glyphicon-tasks"></span>About Us</a>
+                <a class="nav-item nav-link" href="contact.php"><span class="glyphicon glyphicon-phone"></span>Conatct Us</a>
+            </div>
+        </div>
+        </nav>
+<!--------------------------------- NAVBAR End --------------------------------->
--- index.php
+++ index.php
@@ -3,19 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-        <!-- Bootstrap Core CSS -->
-        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <!---------------------------------- SOME CUSTOM LINKS --------------------------->
+        
+            <!-- Bootstrap link -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+            <!-- Bootstrap Core CSS -->
+            <link href="css/bootstrap.css" rel="stylesheet">
+            <!-- jQuery -->
+              <script src="js/jquery.js"></script>
+            <!-- Bootstrap Core JavaScript -->
+              <script src="js/bootstrap.min.js"></script>
+           
+          <!---------------------------------- SOME CUSTOM LINKS --------------------------->
         <!-- Custom CSS -->
-          <link href="./css/style.css" rel="stylesheet">
-        <!-- jQuery -->
-          <script src="js/jquery.js"></script>
-        <!-- Bootstrap Core JavaScript -->
-          <script src="js/bootstrap.min.js"></script>
+          <link rel="stylesheet" href="css/stylesheet.css">
 
     <title>E-store</title>
 </head>
 <body>
+  <?php 
+    include 'includes/header.php';
+  ?>
     
 </body>
 </html>
