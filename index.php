e304b70565a2beedb8176893ebc1f573f7de98ab
Aneesh <srivastavaaneesh004@gmail.com>
2020-12-30 22:46:41 +0530

    Imported bootstrap and Jquery

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+          <link href="./css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+          <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+          <script src="js/bootstrap.min.js"></script>
+
+    <title>E-store</title>
+</head>
+<body>
+    
+</body>
+</html>
