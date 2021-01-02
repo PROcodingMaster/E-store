1289a0e4e825d7e9656ac774bfbdeb53b7be0301
Aneesh <srivastavaaneesh004@gmail.com>
2021-01-02 14:25:51 +0530

    Check if the entered email exist in DB or NOT

--- home.php
+++ home.php
@@ -7,6 +7,16 @@
     $city =  mysqli_real_escape_string($con, $_POST['City']);
     $address =  mysqli_real_escape_string($con, $_POST['Address']);
 
+    //Check if the entered email exist in DB or NOT ------------
+    $email_check_query = "SELECT id FROM users WHERE email_id = '$email'";
+    $email_check_result = mysqli_query($con, $email_check_query);
+    $number_of_email = mysqli_num_rows($email_check_result);
+    if ($number_of_email >0){
+        echo "<h1>Email already exist.</h1>";
+        echo '<h3><a href = "signup.php">click here</a> to go back</h3>';
+        die(mysqli_error($con));
+    }
+
     // Check if all the fields are filled
     $required = array($name, $email, $password, $contact,$city,$address);
     foreach ($required as $field){
